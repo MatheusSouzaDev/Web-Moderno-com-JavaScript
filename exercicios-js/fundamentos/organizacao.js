69dcf91471619f677e2076c5df5f26357c07c2bb
Matheus Souza <matheuswassouza29@gmail.com>
2018-02-23 14:34:58 -0300

    S03EP10. Organização Básica de um Código JS

---
+++ exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+console.log("Sentença de código"); // Sentença de Código
+
+{
+    // Bloco de Código
+    { // Um bloco dentro do outro
+        console.log("Olá");
+        console.log('Mundo!') // Padrão do curso sem ';' 
+    }
+
+}
