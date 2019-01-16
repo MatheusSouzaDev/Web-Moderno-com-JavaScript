5385767df3c418d458c9cb11f04a8ef5364877a6
Matheus de Souza <matheus.zhsouza@gmail.com>
2019-01-16 15:07:26 -0200

    65. Parâmetro Padrão

--- exercicios-js/funcao/paramPadrao.js
+++ exercicios-js/funcao/paramPadrao.js
@@ -1,24 +1,27 @@
 // estrategia 1 para gerar valor padrão
-function soma1(a, b, c) {
+// 0 é considerado valor falso
+function soma1(a, b, c) { // operador OU
     a = a || 1;
     b = b || 1;
     c = c || 1;
-    return a + b + c;
+    return a + b + c; // valores já atribuidos 
 }
 
 console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0));
 
 // estrategia 2, 3 e 4 para gerar valor padrão
+// se passar valor 0 com essas operações ele é identificado como valor true
 function soma2(a, b, c) {
-    a = a !== undefined ? a : 1;
-    b = 1 in arguments ? b : 1;
-    c = isNaN(c) ? 1 : c;
+    a = a !== undefined ? a : 1; // utilizar valor ternário ? : 
+    b = 1 in arguments ? b : 1; // se existir o segundo índice 
+    c = isNaN(c) ? 1 : c; // se não for um número
     return a + b + c;
 }
 
 console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0));
 
 // valor padrão do es2015
+// reduzindo a forma de escrever
 function soma3(a = 1, b = 1, c = 1) {
     return a + b + c;
 }
