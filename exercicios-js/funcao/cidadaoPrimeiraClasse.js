3ccc6aa87bfea09cf4c491991131e9607c874f85
Matheus de Souza <matheus.zhsouza@gmail.com>
2019-01-16 14:24:21 -0200

    62.Cidadão de Primeira Linha

--- exercicios-js/funcao/cidadaoPrimeiraClasse.js
+++ exercicios-js/funcao/cidadaoPrimeiraClasse.js
@@ -3,25 +3,28 @@
 
 
 // criar de forma literal
-function fun1() {}
+// retorna udefined se não retornar valor
+// função não pode omitir os blocos de código
+function fun1() {} 
 
 // Armazenar em uma variável
+// Função anônima ()
 const fun2 = function () {}
 
 // Armazenar em um array
 const array = [function (a, b) {
     return a + b
 }, fun1, fun2]
-console.log(array[0](2, 3));
+console.log(array[0](2, 3)); //executando a função pelo índice
 
 // Armazenar em um atributo de objeto
 const obj = {}
 obj.falar = function () {
     return 'Opa'
 }
-console.log(obj.falar());
+console.log(obj.falar()); // executando a função do objeto
 
-// Passar função como parametro
+// Passar função como parametro para executar ela
 function run(fun) {
     fun();
 }
@@ -37,6 +40,7 @@ function soma(a, b) {
     }
 }
 
-soma(2, 3)(4);
-const cincoMais = soma(2, 3);
-cincoMais(4);
+soma(2, 3)(4); // executando a função e rodando a função de retorno
+
+const cincoMais = soma(2, 3); // declarando uma contante com a função
+cincoMais(4); // executando a função de retorno
