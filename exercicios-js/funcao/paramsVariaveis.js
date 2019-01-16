636397c53a52b3d5657b7532f9cbefa3322fed72
Matheus de Souza <matheus.zhsouza@gmail.com>
2019-01-16 14:49:24 -0200

    64. Parâmetros Variáveis

--- exercicios-js/funcao/paramsVariaveis.js
+++ exercicios-js/funcao/paramsVariaveis.js
@@ -1,7 +1,7 @@
 function soma() {
     let soma = 0;
-    for (let i in arguments) {
-        soma += arguments[i];
+    for (let i in arguments) { // pegar o indice do array arguments
+        soma += arguments[i]; // executando a função com os parâmetros passados
     }
     return soma;
 }
@@ -9,5 +9,6 @@ function soma() {
 console.log(soma());
 console.log(soma(1));
 console.log(soma (1.1, 2.2, 3.3));
-console.log(soma(1.1, 2.2, "Teste"));
-console.log(soma('a', 'b', 'c'));
+console.log(soma(1.1, 2.2, "Teste")); // soma primeiro os valores e depois concatena a string
+console.log(soma('a', 'b', 'c')); // apenas concatena
+console.log(soma(1, 'teste', 2)); // somou o 0+1 e concatenou o resto
