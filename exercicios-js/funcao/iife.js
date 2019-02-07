543e30225cea28df2b36b48b68039915ee030ff5
Matheus de Souza <matheus.zhsouza@gmail.com>
2019-02-07 17:24:11 -0200

    84. IIFE

---
+++ exercicios-js/funcao/iife.js
@@ -0,0 +1,5 @@
+//IIFE -> Immediately Invoked Function Expression (Expressão de Função Imediatamente Invocada)
+(function () {
+	console.log('Será executada na hora!')
+	console.log('Foge do escopo mais abrangente!')
+})() //padrão utilizado para fugir do escopo global
