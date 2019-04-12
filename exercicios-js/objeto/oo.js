ba7c62134c2934fd122aaad49f9f94e1e4124ba4
Matheus de Souza <matheus.zhsouza@gmail.com>
2019-04-12 17:34:00 -0300

    86. Introdução à OO - parte 2

--- exercicios-js/objeto/oo.js
+++ exercicios-js/objeto/oo.js
@@ -18,3 +18,4 @@ objeto.processamento() // Foco mudou de funções para ser o objeto
 // Principios importantes:
 // 1. Abstração => Pegar um objeto do mundo real e introduzir ao sistema CARRO = {Atributos: peso, altura, velocidade máxima, Métodos: ligar, acelerar, parar}
 // 2. Encapsulamento => esconder os detales da implementação e mostrar uma interface simples para interagir com o obejto. Ex.: conhecer que o automóvel precisa de combustível para andar, mas não precisa ter conhecimento de como o motor usa o combustível para movimentar.
+// 
