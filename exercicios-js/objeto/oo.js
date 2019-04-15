a358f0386a2c5751efd3912f1720563e7cf49a6a
Matheus de Souza <matheus.zhsouza@gmail.com>
2019-04-15 08:51:38 -0300

    86. Introdução à OO - parte 3

--- exercicios-js/objeto/oo.js
+++ exercicios-js/objeto/oo.js
@@ -17,5 +17,11 @@ objeto.processamento() // Foco mudou de funções para ser o objeto
 
 // Principios importantes:
 // 1. Abstração => Pegar um objeto do mundo real e introduzir ao sistema CARRO = {Atributos: peso, altura, velocidade máxima, Métodos: ligar, acelerar, parar}
-// 2. Encapsulamento => esconder os detales da implementação e mostrar uma interface simples para interagir com o obejto. Ex.: conhecer que o automóvel precisa de combustível para andar, mas não precisa ter conhecimento de como o motor usa o combustível para movimentar.
-// 
+// 2. Encapsulamento => esconder os detales da implementação e mostrar uma interface simples para interagir com o obejto. Ex.: conhecer que o automóvel precisa de combustível para andar, mas não precisa ter conhecimento de como o motor usa o combustível para movimentar. 
+// Esconder a implementação -> Expor a interface de comunicação
+// 3. Herança (prototype) => 3 tipos
+// * Composição: Carro tem um motor, não precisa colocar a complexidade do motor no objeto carro mas criar um objeto chamado motor que vai estar compondo o carro
+// * Herança: tomar características, comportamentos e atributos dos objetos pais. JS não permite multiplos pais. 
+// Objeto Pai: Civic é um carro.
+// Objeto Filho: O carro tem um motor, o carro tem portas. 
+// 4. Polimorfismo => Através de um conceito genérico pode se tornar algo mais abstrato. Um carro pode ser apontado para uma ferrari, ou uno.
