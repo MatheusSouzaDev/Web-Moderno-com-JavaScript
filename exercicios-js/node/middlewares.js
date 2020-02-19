f32027da5fd8372be4ce227169f8d94613ed8e5a
Matheus de Souza <matheus.zhsouza@gmail.com>
2020-02-19 08:32:53 -0300

    135. Frameworks Web 136. Padrão Middleware #01 137. Padrão Middleware #02 138. Instalando o Postman 139. Projeto: API com Express #01 140. Projeto: API com Express #02 141. Projeto: API com Express #03 142. Tarefas Agendadas com Temporizador 143. Process: Entrada e Saída Padrão

---
+++ exercicios-js/node/middlewares.js
@@ -0,0 +1,34 @@
+const passo1 = (ctx, next) => {
+	ctx.valor1 = 'mid1'
+	next()
+}
+
+const passo2 = (ctx, next) => {
+	ctx.valor2 = 'mid2'
+	next()
+}
+
+const passo3 = ctx => ctx.valor3 = 'mid3'
+
+const exec = (ctx, ...middlewares) => {
+	const execPasso = indice => {
+		middlewares && indice < middlewares.length &&
+			middlewares[indice](ctx, () => execPasso(indice + 1))
+	}
+	execPasso(0)
+}
+
+const ctx = {}
+const ctx1 = {}
+const ctx2 = {}
+const ctx3 = {}
+
+exec(ctx, passo1, passo2, passo3)
+exec(ctx1, passo2, passo1, passo3)
+exec(ctx2, passo1, passo3)
+exec(ctx3, passo3, passo1, passo2)
+
+console.log(ctx)
+console.log(ctx1)
+console.log(ctx2)
+console.log(ctx3)
---
+++ exercicios-js/node/projeto/src/servidor.js
@@ -0,0 +1,44 @@
+const porta = 3003;
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const bancoDeDados = require("./bancoDeDados");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/produtos", (req, res, next) => {
+    res.send(bancoDeDados.getProdutos()); // Converte para JSON automaticamente
+});
+
+app.get("/produtos/:id", (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post("/produtos", (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    });
+
+    res.send(produto); // Objeto para JSON
+});
+
+app.put("/produtos/:id", (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    });
+
+    res.send(produto); // Objeto para JSON
+});
+
+app.delete("/produtos/:id", (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id);
+    res.send(produto); // JSON
+});
+
+app.listen(porta, () => {
+    console.log(`Server is executing in port ${porta}.`);
+});
