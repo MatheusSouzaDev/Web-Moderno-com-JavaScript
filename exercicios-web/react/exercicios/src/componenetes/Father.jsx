90522b6da6f8f7b19cae511096541499018f6d81
Matheus Souza <matheus.zhsouza@gmail.com>
2021-04-12 19:27:29 -0300

    377. Componentes: Pai & Filho #02

--- exercicios-web/react/exercicios/src/componenetes/Father.jsx
+++ exercicios-web/react/exercicios/src/componenetes/Father.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import Son from "./Son"
+import { childrenWithProps } from "../utils/utils";
 
-export default props =>
-<div>
-    <h1>{props.name} {props.lastName}</h1>
+export default (props) => (
+  <div>
+    <h1>
+      {props.name} {props.lastName}
+    </h1>
     <h2>Filhos</h2>
     <ul>
-        <Son name="Pedro" lastName={props.lastName} />
-        <Son {...props} />
-        <Son {...props} name="Carla" />
+      {/* ??? */}
+      {
+        // props.children
+        childrenWithProps(props)
+      }
     </ul>
-</div>
+  </div>
+);
--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 // import First from './componenetes/First'
 // import GoodMorning from './componenetes/GoodMorning'
 // import Multi, { GoodNight} from './componenetes/Multiples'
 // import Greeting from './componenetes/Greeting'
-import Father from './componenetes/Father'
+import Father from "./componenetes/Father";
+import Son from "./componenetes/Son";
 
 ReactDOM.render(
-    <div>
-        <Father name="Paulo" lastName="Silva" >
-            {/* Como passar os componentes Filhos aqui? */}
-            
-        </Father>
-    </div>
-,document.getElementById('root'))
+  <div>
+    <Father name="Paulo" lastName="Silva">
+      {/* Como passar os componentes Filhos aqui? */}
+      <Son name="Pedro" />
+      <Son name="Paulo" />
+      <Son name="Carla" />
+    </Father>
+  </div>,
+  document.getElementById("root")
+);
