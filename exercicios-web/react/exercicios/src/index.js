3d4696cb51f58870a7cf4c627dae981a2a64ad35
Matheus Souza <matheus.zhsouza@gmail.com>
2021-04-12 10:39:06 -0300

    371. Primeiro Componente

--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// $('<h1>').html('React 2')
-const elemento = <h1>React 2</h1>
-ReactDOM.render(elemento, document.getElementById('root'))
+import First from './componenetes/First'
+
+ReactDOM.render(<First />, document.getElementById('root'))
