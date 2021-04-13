7547f44962a0af8ace0f2078b01708b26897fde9
Matheus Souza <matheus.zhsouza@gmail.com>
2021-04-12 22:09:35 -0300

    387. Criando Componentes (Template)

---
+++ exercicios-web/react/crud/frontend/src/components/templates/Logo.jsx
@@ -0,0 +1,4 @@
+import "./Logo.css";
+import React from "react";
+
+export default (props) => <asside className="logo">Logo</asside>;
---
+++ exercicios-web/react/crud/frontend/src/components/templates/Nav.jsx
@@ -0,0 +1,4 @@
+import "./Nav.css";
+import React from "react";
+
+export default (props) => <asside className="menu-area">Navegação</asside>;
