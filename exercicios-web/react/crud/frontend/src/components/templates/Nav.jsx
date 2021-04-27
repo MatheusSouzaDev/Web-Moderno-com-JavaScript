0272ebb01ac094c64e13a6dacc030f14ca862f34
Matheus Souza <matheus.zhsouza@gmail.com>
2021-04-27 20:27:25 -0300

    392. Componente Navegação

--- exercicios-web/react/crud/frontend/src/components/templates/Nav.jsx
+++ exercicios-web/react/crud/frontend/src/components/templates/Nav.jsx
@@ -1,4 +1,12 @@
 import "./Nav.css";
+import NavItem from './NavItem'
 import React from "react";
 
-export default (props) => <aside className="menu-area">Navegação</aside>;
+export default (props) => (
+  <aside className="menu-area">
+    <nav className="menu">
+      <NavItem href="#/" icon="home" title="Início"/>
+      <NavItem href="#/users" icon="users" title="Usuários"/>
+    </nav>
+  </aside>
+);
